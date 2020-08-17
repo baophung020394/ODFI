5882eaf620d9906e7c425f2ce25d67fd6d8e580c
unknown <bao.future94@gmail.com>
2020-08-18 06:52:54 +0700

    aaa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,30 @@
 /** ODEFI Javascript */
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
+// const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+// const currentTheme = localStorage.getItem('theme');
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+// if (currentTheme) {
+//     document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
-}
+//     if (currentTheme === 'dark') {
+//         toggleSwitch.checked = true;
+//     }
+// }
 
-function switchTheme(e) {
-    console.log('doi', e.target.checked)
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-    }
-}
+// function switchTheme(e) {
+//     console.log('doi', e.target.checked)
+//     if (e.target.checked) {
+//         document.documentElement.setAttribute('data-theme', 'dark');
+//         localStorage.setItem('theme', 'dark');
+//     }
+//     else {
+//         document.documentElement.setAttribute('data-theme', 'light');
+//         localStorage.setItem('theme', 'light');
+//     }
+// }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+// toggleSwitch.addEventListener('change', switchTheme, false);
+$(document).ready(function () {
+    $('#example').DataTable({
+        "order": []
+    });
+});
