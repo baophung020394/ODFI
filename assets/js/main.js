3bd7d3c938667e1cfb30399a062d6ef39a4ada00
unknown <bao.future94@gmail.com>
2020-08-19 06:34:01 +0700

    asdasd

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@
 
 // toggleSwitch.addEventListener('change', switchTheme, false);
 
-function openCity(evt, cityName) {
+function openBuySell(evt, tabname) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -34,12 +34,39 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabname).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function openTabBottom(evt, tabname) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent-bottom");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks-bottom");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabname).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
 $(document).ready(function () {
     $('#example').DataTable({
         "order": []
+    });
+    $('#your #table-bottom').DataTable({
+        "order": []
+    });
+    $('#all #table-bottom').DataTable({
+        "order": []
+    });
+    $('#odfi #table-bottom').DataTable({
+        "order": []
+    });
+    $('.table-market #table-bottom').DataTable({
+        // "order": [],
+        responsive: true
     });
 });
