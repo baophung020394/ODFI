05a2ccafffa87e68d6e35320b40cbba351e3bddd
screen <bao.future94@gmail.com>
2020-08-18 02:13:45 -0700

    asdsad

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,20 @@
 // }
 
 // toggleSwitch.addEventListener('change', switchTheme, false);
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 $(document).ready(function () {
     $('#example').DataTable({
         "order": []
